e5c27800405e620ed0ae9f8762a883baf8038b21
mathkraemer <43319514+mathkraemer@users.noreply.github.com>
2022-08-02 13:32:28 +0200

    Trigger another run...

--- src/main/java/org/thb/webshop/util/DataBaseUtil.java
+++ src/main/java/org/thb/webshop/util/DataBaseUtil.java
@@ -445,7 +445,7 @@ public class DataBaseUtil {
 
 	/**
 	 * Deliberate bug
-         * with another irrelevant change
+         * with another 1st irrelevant change
 	 * @return the con
 	 */
 	Connection getCon01(String url) throws SQLException {
