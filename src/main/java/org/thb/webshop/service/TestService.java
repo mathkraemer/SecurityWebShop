9bfa81ecb7604a656d5eeee44a661e36f75ffc7a
Matthias Kraemer <matthias.kraemer@synopsys.com>
2023-04-16 14:56:42 +0200

    Introduced TestService

---
+++ src/main/java/org/thb/webshop/service/TestService.java
@@ -0,0 +1,23 @@
+package org.thb.webshop.service;
+
+public class TestService implements Runnable {
+
+    private String group = 'unknown';
+
+    public TestService(String testgroup){
+        this.group = testgroup;
+    }
+
+    public void run(){
+        
+        int len;
+
+        if (group == null){
+            len = group.length();
+        } else {
+            len = 0;
+        }
+
+    }
+    
+}
